1714aab6cdad4b25cf19be803cf07c5eb11ae47f
Chamarr Auber <chamarrball@gmail.com>
2024-04-03 12:06:34 -0400

    Add files via upload

---
+++ p3-crypto/task0/task0_dec.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python3
+
+import sys
+
+# Read task0_enc.py for explanations for this first part.
+ciphertext = ''
+with open(sys.argv[1]) as f:
+    for line in f:
+        ciphertext += line.strip()
+
+nrails = int(sys.argv[2])
+
+rails = [''] * nrails
+
+ciphertext_len = len(ciphertext)
+
+seq_length = 2 * (nrails - 1)
+
+# We'll need to know how many full sequences we have, and how much
+# of a partial sequence at the end.
+full_seqs = int(ciphertext_len / seq_length)
+seq_remainder = ciphertext_len % seq_length
+
+for i in range(0,nrails):
+    n = full_seqs
+    if i > 0 and i < nrails-1:
+        n *= 2
+    if seq_remainder > i - 1:
+        n += 1
+    if seq_remainder > seq_length - i:
+        n += 1
+    rails[i],ciphertext = ciphertext[0:n],ciphertext[n:]
+
+rindex = [0] * seq_length
+for i in range(0,nrails):
+    rindex[i] = i
+for i in range(nrails, seq_length):
+    rindex[i] = seq_length - i
+
+
+# Now, let's iterate over the input.
+plaintext = ''
+rail_idxs = [0] * nrails
+for i in range(ciphertext_len):
+    # TODO: finish up this loop
+    curr_rail = rindex[i%seq_length]
+    plaintext += rails[curr_rail][rail_idxs[curr_rail]]
+    rail_idxs[curr_rail] += 1
+
+
+print(plaintext)
+
